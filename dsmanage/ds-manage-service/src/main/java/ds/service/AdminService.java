daa131a76944548d597083b2fd9385f75674e346
HELLHELLO <498523614@qq.com>
2019-03-06 16:11:54 +0800

    平台管理员相关功能增加了对密码的RSA加解密

--- dsmanage/ds-manage-service/src/main/java/ds/service/AdminService.java
+++ dsmanage/ds-manage-service/src/main/java/ds/service/AdminService.java
@@ -4,8 +4,8 @@ import ds.common.pojo.Result;
 import ds.pojo.Admin;
 
 public interface AdminService {
-    public Result addAdmin(Admin admin);
+    public Result addAdmin(Admin admin) throws Exception;
     public Result delAdmin(Long id);//根据管理员id删除管理员
-    public Result updateAdmin(Admin admin);//更新管理员信息
+    public Result updateAdmin(Admin admin) throws Exception;//更新管理员信息
     public Result searchAdmin(Admin admin);//查找管理员
 }
--- dsmanage/ds-manage-service/src/main/java/ds/service/impl/AdminServiceImpl.java
+++ dsmanage/ds-manage-service/src/main/java/ds/service/impl/AdminServiceImpl.java
@@ -5,9 +5,18 @@ import ds.mapper.AdminMapper;
 import ds.pojo.Admin;
 import ds.pojo.AdminExample;
 import ds.service.AdminService;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Decoder;
 
+import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.List;
 
 @Service
@@ -16,13 +25,24 @@ public class AdminServiceImpl implements AdminService {
     @Autowired
     private AdminMapper adminMapper;
 
+    @Value("${PRIVATEKEY}")
+    private String privateKeyStr;
+
+    private static RSAPrivateKey privateKey;
+
     @Override
-    public Result addAdmin(Admin admin) {
+    public Result addAdmin(Admin admin) throws Exception{
         if (admin.getAdminName()==null||admin.getAdminPassword()==null){
             return new Result(Result.Status.emptyParam,"empty name or password");
         }
         admin.setAdminId(null);
         admin.setValued(null);
+        String password=admin.getAdminPassword();
+
+        //解密
+        admin.setAdminPassword(decrypt(admin.getAdminPassword()));
+
+
         Integer num=adminMapper.insertSelective(admin);
         if (num==0){
             return new Result(Result.Status.somethingWrong,"insert fail");
@@ -46,11 +66,16 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public Result updateAdmin(Admin admin) {
+    public Result updateAdmin(Admin admin) throws Exception {
         if (admin.getAdminId()==null){
             return new Result(Result.Status.emptyParam,"empty id");
         }
         admin.setValued(null);
+
+        //解密
+        admin.setAdminPassword(decrypt(admin.getAdminPassword()));
+
+
         Integer num=adminMapper.updateByPrimaryKeySelective(admin);
         if (num==0){
             return new Result(Result.Status.somethingWrong,"update fail");
@@ -69,6 +94,24 @@ public class AdminServiceImpl implements AdminService {
             criteria.andAdminNameLike(admin.getAdminName());
         }
         List<Admin> list=adminMapper.selectByExample(adminExample);
+        for (Admin result:list){
+            result.setAdminPassword(null);
+        }
         return new Result(Result.Status.success,"success",list);
+    }
+
+    private String decrypt(String password) throws Exception{
+        //解密
+        Security.addProvider(new BouncyCastleProvider());
+        Cipher cipher=Cipher.getInstance("RSA/None/PKCS1Padding","BC");
+        KeyFactory keyFactory=KeyFactory.getInstance("RSA","BC");
+        if (privateKey==null) {
+            BASE64Decoder base64Decoder = new BASE64Decoder();
+            byte[] buffer = base64Decoder.decodeBuffer(privateKeyStr);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            privateKey=(RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        }
+        cipher.init(Cipher.DECRYPT_MODE,privateKey);
+        return new String(cipher.doFinal(password.getBytes()));
     }
 }
